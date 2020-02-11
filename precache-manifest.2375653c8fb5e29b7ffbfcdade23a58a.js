c799aaa7116f09eef65ede99c6603b3da53ec1fc
divyeshrana9775 <divyeshrana9775@gmail.com>
2020-02-11 16:38:42 -0500

    Updates

---
+++ precache-manifest.2375653c8fb5e29b7ffbfcdade23a58a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccfa86f862a57e5845088da5af60e85",
+    "url": "/mytipapp/index.html"
+  },
+  {
+    "revision": "611f5d2beed657be921f",
+    "url": "/mytipapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fe705aa878e3bbed029c",
+    "url": "/mytipapp/static/js/2.08ad008d.chunk.js"
+  },
+  {
+    "revision": "94b03e19a4c9eb66db847f3cca1e03ba",
+    "url": "/mytipapp/static/js/2.08ad008d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "611f5d2beed657be921f",
+    "url": "/mytipapp/static/js/main.4f8bb603.chunk.js"
+  },
+  {
+    "revision": "c9979696a22c36b6353a",
+    "url": "/mytipapp/static/js/runtime-main.0d461e14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mytipapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
